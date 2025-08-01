28e3255e329aa5f4f74d39e959527107ea8e296b
Tim Heuer <tim@timheuer.com>
2025-07-31 18:07:50 -0700

    Initial

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassLibrary1;
+
+public class Class1
+{
+    public List<string> _items;
+
+    public Class1()
+    {
+        _items = new List<string>();
+    }
+
+    public void AddItem(string item)
+    {
+        _items.Add(item);
+    }
+
+    public bool RemoveItem(string item)
+    {
+        return _items.Remove(item);
+    }
+
+    public List<string> GetItems()
+    {
+        return new List<string>(_items);
+    }
+}
